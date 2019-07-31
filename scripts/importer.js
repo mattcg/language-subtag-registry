8e2e671f0501c90f20b215392faf889a77ee2976
Matthew Caruana Galizia <mattcg@gmail.com>
2019-07-31 11:45:07 +0200

    Update email address

--- scripts/importer.js
+++ scripts/importer.js
@@ -1,5 +1,5 @@
 /**
- * @author Matthew Caruana Galizia <m@m.cg>
+ * @author Matthew Caruana Galizia <mattcg@gmail.com>
  * @license MIT: http://mattcg.mit-license.org/
  * @copyright Copyright (c) 2013, Matthew Caruana Galizia
  */
