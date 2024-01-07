f4a01f6d256f8ef4cc5a8fea27a9e626e10650f1
Ramazan Sancar <ramazansancar4545@gmail.com>
2024-01-07 16:59:45 +0000

    upd: script fixed.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git config --global user.email $mail
 git config --global user.name $name
 git remote set-url origin https://$username:$token@github.com/$username/SansGirisimBayiListesi.git
-if git status --porcelain | grep -q '*'; then 
+if git status --porcelain | grep -q 'data*'; then 
 	git add --all data/json/
 	git add scripts/cache/language-subtag-registry
 	git commit -am '📑 Update: Updated data'
