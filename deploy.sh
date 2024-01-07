bf0db9108114f1b07977cfbc4fee631d2c489bc1
Ramazan Sancar <ramazansancar4545@gmail.com>
2024-01-07 16:57:52 +0000

    add: Script written for automatic update

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+git config --global user.email $mail
+git config --global user.name $name
+git remote set-url origin https://$username:$token@github.com/$username/SansGirisimBayiListesi.git
+if git status --porcelain | grep -q '*'; then 
+	git add --all data/json/
+	git add scripts/cache/language-subtag-registry
+	git commit -am '📑 Update: Updated data'
+	echo $username | echo $token | git push -f origin HEAD:master
+else
+	echo 'No changes detected'
+fi
